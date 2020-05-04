f17d5cb8d2abf92ec3bc067da84d8db610d5a66f
yokoto <yo44ko55@gmail.com>
2020-05-04 13:58:33 +0900

    検索結果のResultコンポーネントを表示する

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import addDays from 'date-fns/addDays';
 import axios from 'axios';
 import format from 'date-fns/format';
+import Result from './Result';
 
 const Today = new Date();
 registerLocale('ja', ja);
@@ -70,6 +71,9 @@ class Home extends React.Component {
               </button>
             </div>
           </form>
+          <Result
+            plans={this.state.plans}
+          />
         </div>
       </div>
     );
---
+++ src/components/Result.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Common.css";
+
+const Result = ({ plans }) => {
+  if (!plans) {
+    return <div></div>;
+  }
+
+  const results = plans.map(plan => {
+    return (
+      <div className="item" key={plan.plan_id}>
+        <div className="image">
+          <img src={plan.image_url} alt={plan.course_name} />
+        </div>
+        <div className="content">
+          <div className="meta">
+            <span className="cinema">{plan.course_name}</span>
+            <div className="ui mini statistics">
+              <div className="statistic">
+                <div className="value">
+                  <i className="car icon"></i> {plan.duration + "分"}
+                </div>
+              </div>
+              <div className="statistic">
+                <div className="value">
+                  <i className="yen sign icon"></i> {plan.price.toLocaleString()}
+                </div>
+              </div>
+              <div className="statistic">
+                <div className="value">
+                  <i className="thumbs up outline icon"></i> {plan.evaluation}
+                </div>
+              </div>
+            </div>
+            <div className="ui star rating" data-rating="3"></div>
+            <div className="extra">
+              <div className="ui label">{plan.prefecture}</div>
+              <div className="ui label">{plan.plan_name}</div>
+            </div>
+            </div>
+            <div className="description">
+              <p>{plan.caption}</p>
+            </div>
+            <div className="item-button">
+              <a
+                href={plan.reserve_url_pc}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                コースの予約はこちら
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+  return <div>{results}</div>;
+};
+
+export default Result;
